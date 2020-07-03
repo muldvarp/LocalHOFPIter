5c5c8a49971a2971eacf4c507c91542fce830993
mlange <martin.lange@uni-kassel.de>
2020-07-03 17:08:44 +0200

    replaced old example 3

--- example3.ml
+++ example3.ml
@@ -1,113 +1,84 @@
+(*** The model checking example ***)
+
 open HOFPIteration
 
-module IntSet = Set.Make  ( struct 
-                              type t = int
-                              let compare = compare
-                            end
-                          )
+let n = ref 2
 
-let n = 1
+let _ =
+  n := (try
+          int_of_string Sys.argv.(1)
+        with _ -> 2)
 
 module PowerSetLattice =
   struct
 
-    type t = IntSet.t
+    type t = bool array
     
-    let equal = IntSet.equal
+    let equal = (=)
 
     let show set = 
-       "{" ^ (String.concat "," (List.map (fun elem -> string_of_int elem) (IntSet.elements set))) ^ "}"
+       "{" ^ String.concat "," (List.filter (fun s -> s <> "") (Array.to_list (Array.mapi (fun i -> fun b -> if b then string_of_int i else "") set))) ^ "}"
     
-    let top =
-      let range a b =
-        let rec aux acc high low =
-          if high >= low then
-            aux (high::acc) (high - 1) low
-          else acc
-        in if a < b then aux [] b a else List.rev (aux [] a b)
-      in
-        IntSet.of_list (range 0 n)
+    let top = Array.make !n true
     
-    let bot =
-        IntSet.empty
+    let bot = Array.make !n false
     
-    let size _ = int_of_float (2. ** ((float) (n+1)) )
-    let height _ = n+2
+    let size _ = int_of_float (2. ** ((float) (!n)) )
+    let height _ = !n + 1
     
-    let p _ = IntSet.add 0 (IntSet.empty) 
+    let p _ = Array.init !n (fun i -> i=0)  
     
-    let first = IntSet.empty
+    let first = bot
 
-    let list_of_all_subsets = 
-      let rec superset_list = function
-      | [] -> [[]]
-      | x :: xs -> 
-         let ps = superset_list xs in
-         ps @ List.map (fun ss -> x :: ss) ps
-      in
-        List.map (fun subset_list -> IntSet.of_list subset_list) (superset_list (IntSet.elements top))
+    let next set = if set=top then None
+                   else 
+                     let change = ref true in
+                     Some (Array.mapi (fun i -> fun b -> if !change then
+                                                     begin
+                                                       change := b;
+                                                       not b
+                                                     end
+                                                   else
+                                                     b) set)
 
-    let next set =  
-      let rec find set lst c = 
-        match lst with
-          | [] -> failwith ("ERROR: subset not found")
-          | hd::tl -> if (IntSet.equal set hd) then c else find set tl (c+1)
-      in
-      let idx = find set list_of_all_subsets 0 in
-      if idx == (size()-1) then
-        None
-      else
-        Some (List.nth list_of_all_subsets (idx+1))
-      
+    let map2 f x y = Array.init !n (fun i -> f x.(i) y.(i))
+                   
+    let disj = function [x;y] -> map2 (||) x y
+                      | _     -> failwith ("ERROR: wrong number of arguments given to function `disj´!")
 
-
-    let disj =  function    [x;y] -> IntSet.union x y
-                        | _     -> failwith ("ERROR: wrong number of arguments given to function `disj´!")
-
-    let conj =  function    [x;y] -> IntSet.inter x y
-                        | _     -> failwith ("ERROR: wrong number of arguments given to function `conj´!")
+    let conj = function [x;y] -> map2 (&&) x y
+                      | _     -> failwith ("ERROR: wrong number of arguments given to function `conj´!")
                         
-    let neg =   function    [x] -> IntSet.diff top x
-                        | _     -> failwith ("ERROR: wrong number of arguments given to function `neg´!")
+    let neg = function [x] -> Array.map not x
+                     | _   -> failwith ("ERROR: wrong number of arguments given to function `neg´!")    
     
-    
-    let a_dia = function    [x]   -> IntSet.fold  (fun elem new_set-> if elem == 0 then 
-                                                                            IntSet.add 1 new_set
-                                                                          else
-                                                                            if elem < n then
-                                                                              IntSet.add (elem-1) (IntSet.add (elem+1) new_set)
-                                                                            else
-                                                                              IntSet.add (elem-1) new_set
-                                                      ) x IntSet.empty   
-                              | _     -> failwith ("ERROR: wrong number of arguments given to function `a-diamond!")
-    
+    let a_dia = function [x] -> (try
+                                  Array.init !n (fun i -> (i>0 && x.(i-1)) || (i< !n-1) && x.(i+1))
+                                with Invalid_argument _ -> failwith "Hier!")
+                       | _   -> failwith ("ERROR: wrong number of arguments given to function `a-diamond!")
+
+    (*
     let a_box = function        [x]   -> neg [a_dia [neg [x]]]
                               | _     -> failwith ("ERROR: wrong number of arguments given to function `a-box!")
-
-    let b_dia = function        [x]   ->  IntSet.fold (fun elem new_set -> 
-                                                    if elem == 0 then
-                                                      IntSet.add 0 (IntSet.add 1 new_set)
-                                                    else
-                                                    if elem < n then
-                                                      IntSet.add (elem+1) new_set
-                                                    else
-                                                      new_set
-                                                    ) x IntSet.empty
-                              | _     -> failwith ("ERROR: wrong number of arguments given to function `b-diamond!")
+     *)
+                                
+    let b_dia = function [x] -> Array.init !n (fun i -> x.(0))
+                       | _   -> failwith ("ERROR: wrong number of arguments given to function `b-diamond!")
     
-
+    (*
     let b_box = function    [x]   -> neg [b_dia [neg [x]]]
                               | _     -> failwith ("ERROR: wrong number of arguments given to function `b-box!")
-
-    let base_funcs = [("p",p);("neg", neg);("disj",disj); ("conj", conj); ("<a>", a_dia); ("[a]", a_box); ("<b>", b_dia); ("[b]", b_box)]
+     *)
+                                       
+    let base_funcs = [("p",p);("neg", neg);("disj",disj); ("conj", conj); ("<a>", a_dia); (*("[a]", a_box);*) ("<b>", b_dia) (*; ("[b]", b_box)*)]
   end
-
-
+  
 module PS = MakeHOLattice(PowerSetLattice)
-
-  let _ =
+          
+let _ =
     let typ0 = grtype in
     let typ1 = FuncType([typ0]) in
+    let typ2 = FuncType([typ1]) in
     let a_dia_func = Lamb(["x"], Appl( Base("<a>"), [(Var("x"), typ0)])) in
     let left_conj = Appl(Var("f"), [(Base("p"),typ0)]) in
     let f_composition = Lamb(
@@ -125,5 +96,5 @@ module PS = MakeHOLattice(PowerSetLattice)
                                   )
                             ) in
     let right_conj = Appl(Base("<b>"),[(Appl(Var("F"),[(f_composition,typ1)]),typ0)]) in
-    let phi = Appl(   Nu("F",typ1,Lamb(["f"], Appl(Base("conj"), [(left_conj,typ0);(right_conj,typ0)]))),    [(a_dia_func,typ1)]) in
+    let phi = Appl(   Nu("F",typ2,Lamb(["f"], Appl(Base("conj"), [(left_conj,typ0);(right_conj,typ0)]))),    [(a_dia_func,typ1)]) in
     PS.evaluate phi
